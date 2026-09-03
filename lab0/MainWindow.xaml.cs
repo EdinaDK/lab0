6b64ad1e63061517b2f9686bcdc2bd80d5939517
з <jhg@ffdf>
2026-09-03 12:23:21 +0700

    добавили методы отрисовки

--- lab0/MainWindow.xaml.cs
+++ lab0/MainWindow.xaml.cs
@@ -16,10 +16,49 @@ namespace lab0
     /// </summary>
     public partial class MainWindow : Window
     {
+        Triangle tr;
+        Random rnd = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
-            //приветики (это первая ветка)
+            //Создание треугольника со случайными координатами
+            Point2D p1 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            tr = new Triangle(p1, p2, p3);
+
+        }
+
+        //функция в основном теле программы
+        public void DrawLine(Point2D p1, Point2D p2)
+        {
+            //Создание новой линии
+            Line line = new Line();
+            //Цвет и толщина линии
+            line.Stroke = Brushes.Red;
+            line.StrokeThickness = 3;
+            //Установка координат линии из координат точек Point2D
+            line.X1 = p1.X;
+            line.Y1 = p1.Y;
+            line.X2 = p2.X;
+            line.Y2 = p2.Y;
+            //Добавление линии в Canvas
+            Scene.Children.Add(line);
+        }
+
+        public void DrawTriangle(Triangle tr)
+        {
+            //Отрисовка треугольника с помощью функции отрисовки линии
+            DrawLine(tr.P1, tr.P2);
+            DrawLine(tr.P2, tr.P3);
+            DrawLine(tr.P3, tr.P1);
+        }
+
+        public void ClearScene()
+        {
+            //Очистка Canvas от всех объектов
+            Scene.Children.Clear();
         }
     }
 }
